a07378180574aa874c5fe384f23966c72740e618
Pedro Santos <pedroamaralsantos@ua.pt>
2015-05-23 17:11:21 +0100

    hexdisplay

---
+++ Code/HexDisplayDecoder/HexDisplay.vhd
@@ -0,0 +1,46 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+
+entity HexDisplay is
+	port(enable     : in std_logic;
+		  error      : in std_logic;
+	     dispOut0_n : out std_logic_vector(6 downto 0);
+		  dispOut1_n : out std_logic_vector(6 downto 0);
+		  dispOut2_n : out std_logic_vector(6 downto 0);
+		  dispOut3_n : out std_logic_vector(6 downto 0);
+		  dispOut4_n : out std_logic_vector(6 downto 0);
+		  dispOut5_n : out std_logic_vector(6 downto 0));
+end HexDisplay;
+
+architecture Structural of HexDisplay is
+
+
+	
+	signal s_registerOut	: std_logic_vector(23 downto 0);
+
+begin
+
+
+	disp_0_decoder : entity WORK.Bin7SegDecoder(Behavioral)
+		port map(binInput		 => s_registerOut(3 downto 0),
+               decOut_n		 => dispOut0_n);
+
+	disp_1_decoder : entity WORK.Bin7SegDecoder(Behavioral)
+		port map(binInput		 => s_registerOut(7 downto 4),
+					decOut_n		 => dispOut1_n);
+
+	disp_2_decoder : entity WORK.Bin7SegDecoder(Behavioral)
+		port map(binInput		 => s_registerOut(11 downto 8),
+					decOut_n		 => dispOut2_n);
+
+	disp_3_decoder : entity WORK.Bin7SegDecoder(Behavioral)
+		port map(binInput		 => s_registerOut(15 downto 12),
+					decOut_n		 => dispOut3_n);
+					
+	disp_4_decoder : entity WORK.Bin7SegDecoder(Behavioral)
+		port map(binInput		 => s_registerOut(19 downto 16),
+					decOut_n		 => dispOut4_n);		
+	disp_5_decoder : entity WORK.Bin7SegDecoder(Behavioral)
+		port map(binInput		 => s_registerOut(23 downto 20),
+					decOut_n		 => dispOut5_n);							
+end Structural;
